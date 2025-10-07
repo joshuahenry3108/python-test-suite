ed7dd7e82c252d632e8fb447d44e83ce12f82a1b
AjithaRQ <ajitha.devi@rootquotient.com>
2025-10-07 19:39:15 +0530

    added files

---
+++ testapp.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_api_returns_hello_message(client):
+    response = client.get("/api")
+    assert response.status_code == 200
+    assert response.is_json
+    data = response.get_json()
+    assert data == {"message": "Hello from Flask!"}
