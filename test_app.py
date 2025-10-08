36069686ae537736b74686e16048a582d134e659
AjithaRQ <ajitha.devi@rootquotient.com>
2025-10-08 11:52:08 +0530

    added failure testcase

--- test_app.py
+++ test_app.py
@@ -7,9 +7,12 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_api_returns_hello_message(client):
-    response = client.get("/api")
-    assert response.status_code == 200
-    assert response.is_json
-    data = response.get_json()
-    assert data == {"message": "Hello from Flask!"}
+# def test_api_returns_hello_message(client):
+#     response = client.get("/api")
+#     assert response.status_code == 200
+#     assert response.is_json
+#     data = response.get_json()
+#     assert data == {"message": "Hello from Flask!"}
+
+def test_failure_example():
+    assert False, "This test is supposed to fail"
